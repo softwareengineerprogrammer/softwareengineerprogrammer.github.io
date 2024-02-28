95346d1b2599a334e1bca478c9459fc98075dea8
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-02-28 08:13:16 -0800

    gh-issue-title-in-url.user.js v1.1

--- userscripts/gh-issue-title-in-url.user.js
+++ userscripts/gh-issue-title-in-url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GH Issue Title in URL
 // @namespace    http://github.com/softwareengineerprogrammer
-// @version      1.0
+// @version      1.1
 // @description  Automatically add descriptive title query parameter to GitHub issue URLs to make them human-readable/referenceable
 // @author       softwareengineerprogrammer
 // @match        https://github.com/*/issues/*
@@ -11,22 +11,49 @@
 // @downloadURL https://softwareengineerprogrammer.github.io/userscripts/gh-issue-title-in-url.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    let titleSnippet = document.querySelector('h1 .markdown-title').innerText.replaceAll(' ', '+').replaceAll('`','')
+    let attempts = 0
+    const MAX_ATTEMPTS = 69
 
-    console.debug('GH issue', titleSnippet)
-    // Construct URLSearchParams object instance from current URL querystring.
-    let queryParams = new URLSearchParams(window.location.search)
+    function setTitleQueryParam() {
+        let issueTitleElt = document.querySelector('h1 .markdown-title')
+        if (issueTitleElt === null) {
+            attempts++
+            if (attempts < MAX_ATTEMPTS) {
+                setTimeout(setTitleQueryParam, 111)
+            } else {
+                console.debug('[gh-issue-title-in-url.user.js] Failed to find issue title element after', MAX_ATTEMPTS, 'attempts, giving up =(')
+            }
+            return
+        }
 
-    // Set new or modify existing parameter value.
-    // queryParams.set("title", titleSnippet)
-    queryParams.set("title", 'GH_ISSUE_TITLE')
-    let queryParamsToString = queryParams.toString()
-    // console.debug('Encoded URL component index in query params.toString()',queryParamsToString.indexOf(encodeURIComponent(titleSnippet)))
-    // console.debug('queryParams title', queryParams.get('title'))
+        let titleSnippet = issueTitleElt.innerText
 
-    // Replace current querystring with the new one.
-    history.replaceState(null, null, "?"+queryParamsToString.replace('GH_ISSUE_TITLE', titleSnippet))
+        const customReplacements = new Map([
+            [' ', '+'],
+            ['`', ''],
+            ['<', 'gt'],
+            ['>', 'lt'],
+            ['℃', 'C']
+        ])
+        customReplacements.forEach(function (value, key, map) {
+            titleSnippet = titleSnippet.replaceAll(key, value)
+        })
+
+
+        let queryParams = new URLSearchParams(window.location.search)
+        queryParams.set("title", 'GH_ISSUE_TITLE')
+        let queryParamsToString = queryParams.toString()
+
+        // console.debug('Encoded URL component index in query params.toString()',queryParamsToString.indexOf(encodeURIComponent(titleSnippet)))
+        // console.debug('queryParams title', queryParams.get('title'))
+
+        let adjustedParams = queryParamsToString.replace('GH_ISSUE_TITLE', titleSnippet)
+        adjustedParams = adjustedParams.replace(/%[0-9][A-F]/g, '')
+        history.replaceState(null, null, "?" + adjustedParams)
+    }
+
+    setTitleQueryParam()
 })();
