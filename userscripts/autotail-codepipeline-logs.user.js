912b62a0e1acbecfbfb4e51ec538127310d65b9e
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-02-10 07:43:55 -0800

    partially fix autotail - FIXME TODO, requires tracking navigation within SPA to work without page refresh

--- userscripts/autotail-codepipeline-logs.user.js
+++ userscripts/autotail-codepipeline-logs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-Tail CodePipeline Logs
 // @namespace    http://softwareengineerprogrammer.github.io
-// @version      0.1
+// @version      0.2
 // @description  un-papercut tail logs
 // @author       softwareengineerprogrammer
 // @match        https://*.console.aws.amazon.com/codesuite/codepipeline/pipelines/*/debug*
@@ -13,7 +13,11 @@
 (function () {
     'use strict';
 
-    let maxTries = 10;
+    function isHidden(el) {
+        return el && (el.offsetParent === null);
+    }
+
+    const maxTries = 50;
     let loop = function (tries) {
         let elt = document.querySelector('.dx-LogTab__header > awsui-button:nth-child(2) > button:nth-child(1) > span:nth-child(1)');
         if (elt) {
@@ -22,6 +26,14 @@
             setTimeout(function () {
                 elt.click();
                 console.debug('Clicked Tail Logs button.');
+
+                setTimeout(function () {
+                    if (isHidden(document.querySelector('div.dx-StatusIndicator:nth-child(1)'))) {
+                        console.debug('Tail Logs pane not showing, resuming search-n-click...');
+                        loop(tries);
+                    }
+                }, 1000);
+
             }, 1000);
         } else if (tries < maxTries) {
             console.debug('Didn\'t find Tail Logs button, will try again...');
