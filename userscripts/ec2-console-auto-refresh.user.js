3e9d58f744a90273a37307934221b27c65a539cc
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-12-24 10:17:56 -0800

    ec2-console-auto-refresh.user.js

---
+++ userscripts/ec2-console-auto-refresh.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         EC2 Console Auto-Refresh
+// @namespace    https://github.com/softwareengineerprogrammer
+// @version      2025-12-24
+// @author       softwareengineerprogrammer
+// @match        https://002349063457-fqlr3kqa.us-west-2.console.aws.amazon.com/ec2/home?region=us-west-2
+// @grant        none
+// @updateURL https://softwareengineerprogrammer.github.io/userscripts/ec2-console-auto-refresh.user.js
+// @downloadURL https://softwareengineerprogrammer.github.io/userscripts/ec2-console-auto-refresh.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(
+        function(){
+            document.querySelector('button[title="Refresh instances"]').click()
+        },
+        30000
+    )
+})();
